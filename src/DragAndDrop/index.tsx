d59327a6a356eb101c6b801ed3923a04d407e131
Unknown <sarobidyalliance@outlook.com>
2022-11-10 14:26:44 +0300

    add link redirection github

--- src/DragAndDrop/index.tsx
+++ src/DragAndDrop/index.tsx
@@ -5,6 +5,7 @@ import { HTML5Backend } from 'react-dnd-html5-backend'
 const DragAndDrop = () => {
     return (
         <div className="App">
+            <p><a href="https://github.com/sarobidy12/easy-react-drag-and-drop"  target="_blank"> Click here </a>, if you want see code source </p>
             <DndProvider backend={HTML5Backend}>
                 <Example />
             </DndProvider>
